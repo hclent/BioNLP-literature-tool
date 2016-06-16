e0fb3b4cfe2bae0517f0f096b527aad82e3949fc
Heather Lent <hclent@email.arizona.edu>
2016-06-15 19:06:51 -0700

    output annotated docs to json

--- 3_cluster/lda1.py
+++ 3_cluster/lda1.py
@@ -3,99 +3,148 @@ from time import time
 from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
 from sklearn.decomposition import LatentDirichletAllocation
 from processors import *
-import re, nltk
+import re, nltk, json
+from json import JSONEncoder
 from nltk.tokenize import RegexpTokenizer
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from sklearn.datasets import fetch_20newsgroups
+import pprint
 
-# source activate pyProcessors #my conda python enviornment
+# source activate pyProcessors #my conda python enviornment for this
 
 # set a PROCESSORS_SERVER environment variable.
 # It may take a minute or so to load the large model files.
-#api = ProcessorsAPI(port=8886)
+api = ProcessorsAPI(port=8886)
+#api.start_server("/Users/hclent/anaconda3/envs/pyProcessors/lib/python3.4/site-packages/processors/processors-server.jar")
 
 
-print("Loading dataset...")
-prefix = "/Users/hclent/Desktop/BioNLP-literature-tool/1_info_retrieval/18952863_"
 
 eng_stopwords = nltk.corpus.stopwords.words('english') #remove default english stopwords 
-# bio_stopwords = ['et', 'al', 'fig', 'author'] #add hand picked bio stopwords to stopwords
-# for word in bio_stopwords:
-# 	eng_stopwords.append(word)
-
-def preProcessing(text):
-	clean_text = re.sub('\\\\n', ' ', text) #replace \n with a space
-	clean_text = re.sub('\([ATGC]*\)', '', clean_text) #delete any DNA seqs
-	clean_text = re.sub('(\(|\)|\'|\]|\[|\\|\,)', '', clean_text) #delete certain punctuation
-	clean_text = clean_text.lower()
-	biodoc = api.bionlp.annotate(clean_text) #str
-	lemmas_list = biodoc.lemmas #list 
-	keep_lemmas = [w for w in lemmas_list if w.lower() not in eng_stopwords]
-	preprocessed_text = (' '.join(map(str, keep_lemmas))) #map to string. strings are necessary for the TFIDF
-	return preprocessed_text
-  #t0 = time()
-	#print("done in %0.3fs." % (time() - t0))
+bio_stopwords = ['et', 'al', 'fig', 'author'] #add hand picked bio stopwords to stopwords
+for word in bio_stopwords:
+	eng_stopwords.append(word)
 
 
+#Input: Data that you want to be JSONified
+#Output: Data reformatted so it can be dumped to JSON
+def dumper(obj):
+  try:
+    return obj.toJSON()
+  except:
+    return obj.__dict__
+
+#Input: String(text), pmid, doc_num
+#Output: This method cleans the text of newline markups, DNA sequences, and some punctuation
+#Output: Then it makes a "biodoc" using the PyProcessor's "BioNLP" Processor. This step takes a while for longer docs
+#Output: This doc is saved to JSON. 
+#Output: pmid and doc_num are for naming the JSON filename 
+def preProcessing(text, pmid, doc_num):
+  print("* Preprocessing the text ... ")
+  clean_text = re.sub('\\\\n', ' ', text) #replace \n with a space
+  clean_text = re.sub('\([ATGC]*\)', '', clean_text) #delete any DNA seqs
+  clean_text = re.sub('(\(|\)|\'|\]|\[|\\|\,)', '', clean_text) #delete certain punctuation
+  clean_text = clean_text.lower()
+  print("* Annotating with the Processors ...")
+  print("* THIS MAY TAKE A WHILE ...")
+  biodoc = api.bionlp.annotate(clean_text) #annotates to JSON
+  print("* Successfully did the preprocessing !!!")
+  print("* Dumping JSON ... ")
+  with open('json_'+(str(pmid))+'_'+str(doc_num)+'.json', 'w') as outfile:
+    json.dump(biodoc, outfile, default=dumper, indent=2)
+  print("* Dumped to JSON !!! ")
+
+
+#Input: filehandle and max number of documents to process
+#Output: JSONified annotated BioDoc 
+## CHANGE: add 'IF' filehandle for BioDoc already exists, skip it
 def loadDocuments(filenamePrefix, maxNum):
-  data_samples = [] #need a list of docs as strings
+  #data_samples = [] #need a list of docs as strings
+  print("* Loading dataset...")
   i = 1
   for i in range(1, maxNum+1):
+    print("* Loading document #" + str(i) + " ...")
     filename = filenamePrefix + str(i) + ".txt"
     text = open(filename, 'r')
     text = text.read()
-    words = preProcessing(text)
-    data_samples.append(words)
+    preProcessing(text,18952863,i)
     i +=1
-  return data_samples
-#
-#data_samples = loadDocuments(prefix, 1) #API connection issues when looping through docs
+    print("\n")
+
+prefix = "/Users/hclent/Desktop/BioNLP-literature-tool/1_info_retrieval/18952863_"
+loadDocuments(prefix, 84) #Randomly chose 4 but I have 84 documents with this prefix
+
+
+# def grab_lemmas(biodoc):
+#   lemmas_list = biodoc["lemmas"] #list 
+#   keep_lemmas = [w for w in lemmas_list if w.lower() not in eng_stopwords]
+#   keep_lemmas = (' '.join(map(str, keep_lemmas))) #map to string. strings are necessary for the TFIDF
+#   return keep_lemmas
+
+# def grab_nes(biodoc):
+#   ners_list = biodoc["nes"] #list 
+#   return ners_list
+
+
+# def loadBioDoc(filenamePrefix, maxNum):
+#   data_samples = []
+#   i = 1
+#   for i in range(1, maxNum+1):
+#     print("* Loading annotated BioDoc from JSON #" + str(i) + " ...")
+#     filename = filenamePrefix + str(i) + ".json"
+#     #print(filename)
+#     with open(filename) as data_file:
+#       data = json.load(data_file)
+#       lemmas = grab_lemmas(data)
+#       data_samples.append(lemmas)
+#     i +=1
+#   return data_samples
 
 
 
-################ LDA ############################################
-doc1 = ['pineapple ananas comosus l. merr. is the most economically', 
-'valuable crop possessing crassulacean acid metabolism cam, a photosynthetic carbon assimilation pathway with high water use efficiency',
-'and the second most important tropical fruit after banana in terms of international trade. we sequenced the genomes of pineapple varieties ‘f153’ and ‘md2’, and a wild pineapple relative a. bracteatus accession cb5. the pineapple genome has one fewer ancient whole genome duplications than sequenced grass genomes and, therefore, provides an important reference for elucidating gene content and structure in the last common ancestor of extant members of the grass family poaceae.',
-'pineapple has a conserved karyotype with seven pre rho duplication chromosomes that are ancestral to extant grass karyotypes. the pineapple lineage has transitioned from c3 photosynthesis to cam with cam-related genes exhibiting a diel expression pattern in photosynthetic tissues using beta-carbonic anhydrase βca for initial capture of co2. promoter regions of all three βca genes contain a cca1 binding site', 
-'that can bind circadian core oscillators. cam pathway genes were enriched with cis-regulatory elements including the morning  and evening  elements associated with regulation of circadian-clock genes, providing the first link between cam and the circadian clock regulation. gene-interaction network analysis revealed both activation and repression of regulatory elements that control key enzymes in cam photosynthesis', 
-'indicating that cam evolved by reconfiguration of pathways preexisting in c3 plants. pineapple cam photosynthesis is the result of regulatory neofunctionalization of preexisting gene copies and not acquisition of neofunctionalized genes via whole genome or tandem gene duplication.       , these authors contributed equally to this work., pineapple ananas comosus l. merr. is the most economically valuable crop possessing crassulacean acid metabolism cam, a photosynthetic carbon assimilation pathway',
-'with high water use efficiency, and the second most important tropical fruit after banana in terms of international trade. we sequenced the genomes of pineapple varieties ‘f153’ and ‘md2’, and a wild pineapple relative a. bracteatus accession cb5. the pineapple genome has one fewer ancient whole genome duplications than sequenced grass genomes and, therefore, provides an important reference for elucidating gene content and structure in the last common ancestor of extant members of the grass family poaceae.',
-'pineapple has a conserved karyotype with seven pre rho duplication chromosomes that are ancestral to extant grass karyotypes. the pineapple lineage has transitioned from c3 photosynthesis to cam with cam-related genes exhibiting a diel expression pattern in photosynthetic tissues using beta-carbonic anhydrase βca for initial capture of co2. promoter regions of all three βca genes contain a cca1 binding site that can bind circadian core oscillators. cam pathway genes were enriched with cis-regulatory elements including the morning  and evening  elements associated with regulation of circadian-clock genes, providing the first link between cam and the circadian clock regulation. gene-interaction network analysis revealed both activation and repression of regulatory elements that control key enzymes in cam photosynthesis, indicating that cam evolved by reconfiguration of pathways preexisting in c3 plants. pineapple cam photosynthesis is the result of regulatory neofunctionalization of preexisting gene copies and not acquisition of neofunctionalized genes via whole genome or tandem gene duplication.']
+#json_prefix = '/Users/hclent/Desktop/BioNLP-literature-tool/3_cluster/json_18952863_'
+#data_samples = loadBioDoc(json_prefix, 4)
 
-def get_tfidf(data): #data should be a list of strings for the documents 
-  print("Making tfidf with the data ...")
-  tfidf_vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1, 2), norm='l2')
-  print("make the vectorizor")
-  tfidf = tfidf_vectorizer.fit_transform(data)
-  print("fit the X")
-  return tfidf, tfidf_vectorizer
+# dump data_samples to pickle so can work on LDA 
 
-def fit_lda(tfidf):
-  lda = LatentDirichletAllocation(n_topics=5, max_iter=5, learning_method='online', learning_offset=50., random_state=0)
-  t0 = time()
-  lda.fit(tfidf)
-  return lda
-  print("done in %0.3fs." % (time() - t0))
+#exit()
 
 
-def print_top_words(model, feature_names, n_top_words):
-  for topic_idx, topic in enumerate(model.components_):
-    print("Topic #%d:" % topic_idx)
-    print(" ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
-  print()
+############################# LDA ############################################
+# dataset = fetch_20newsgroups(shuffle=True, random_state=1, remove=('headers', 'footers', 'quotes'))
+# data_samples = dataset.data
+
+# def get_tfidf(data): #data should be a list of strings for the documents 
+#   print("* Preparing to vectorize data ...")
+#   tfidf_vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1, 2), norm='l2')
+#   print("* Fitting data to vector ...")
+#   tfidf = tfidf_vectorizer.fit_transform(data)
+#   print("* Successfully fit data to the vector !!! ")
+#   return tfidf, tfidf_vectorizer
+
+# def fit_lda(tfidf):
+#   print("* Initializing Latent Dirichlet Allocation ... ")
+#   #Gets super slow here. Why?
+#   lda = LatentDirichletAllocation(n_topics=10, max_iter=5, learning_method='online', learning_offset=50., random_state=1)
+#   lda.fit(tfidf)
+#   print("* Successfully fit data to the model!!! ")
+#   return lda
 
 
-def topics_lda(tf_vectorizer, lda):
-  print("\nTopics in LDA model:")
-  tf_feature_names = tf_vectorizer.get_feature_names()
-  results = print_top_words(lda, tf_feature_names, 3)
-  #print(tf_feature_names)
-  print(results)
+# def print_top_words(model, feature_names, n_top_words):
+#   for topic_idx, topic in enumerate(model.components_):
+#     print("Topic #%d:" % topic_idx)
+#     print(" ".join([feature_names[i] for i in topic.argsort()[:-n_top_words - 1:-1]]))
 
 
-tfidf, tfidf_vectorizer = get_tfidf(doc1)
-print(tfidf)
-lda = fit_lda(tfidf)
-#print(lda)
-topics_lda(tfidf_vectorizer, lda)
+# def topics_lda(tf_vectorizer, lda):
+#   print("\nTopics in LDA model:")
+#   tf_feature_names = tf_vectorizer.get_feature_names()
+#   results = print_top_words(lda, tf_feature_names, 10)
+#   #print(tf_feature_names)
+
+
+# tfidf, tfidf_vectorizer = get_tfidf(data_samples)
+# lda = fit_lda(tfidf)
+# print("############## RESULTS ###############")
+# topics_lda(tfidf_vectorizer, lda)
